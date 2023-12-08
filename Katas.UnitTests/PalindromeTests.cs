d4ba5d01c511ed2da749d142a8b9a11a7bce215f
Matt Conroy <matt@conductr.co.uk>
2023-12-08 09:14:04 +0000

    add Palindromes task

---
+++ Katas.UnitTests/PalindromeTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace Katas.UnitTests;
+
+[TestFixture]
+public class PalindromeTests
+{
+    [TestCase("racecar")]
+    [TestCase("Racecar")]
+    [TestCase("A man, a plan, a canal, Panama")]
+    [TestCase("")]
+    [TestCase(" ")]
+    [TestCase("1234321")]
+    public void GivenInputIsPalindrome_ThenTrueIsReturned(string input)
+    {
+        var result = Palindromes.IsPalindrome(input);
+        Assert.That(result, Is.True);
+    }
+
+    [TestCase("no palindrome")]
+    [TestCase("1234322")]
+    public void GivenInputIsNotPalindrome_ThenFalseIsReturned(string input)
+    {
+        var result = Palindromes.IsPalindrome(input);
+        Assert.That(result, Is.False);
+    }
+}
---
+++ Katas/Palindromes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Katas;
+
+public static class Palindromes
+{
+    /// <summary>
+    ///     A function that takes a string as input and returns true if the string is a palindrome, and false otherwise.
+    ///     A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward
+    ///     (ignoring spaces, punctuation, and capitalization).
+    /// </summary>
+    public static bool IsPalindrome(string str)
+    {
+        throw new NotImplementedException();
+        // Your code here
+    }
+}
